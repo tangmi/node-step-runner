1b2ecbc231f167e84a34ed5f8b35839c8939d683
Michael Tang <tangmi@uw.edu>
2014-04-17 17:57:23 -0700

    Allow selecting scripts

--- bin/index.js
+++ bin/index.js
@@ -1,19 +1,32 @@
 #!/usr/bin/env node
 
+var fs = require('fs'),
+	path = require('path');
+
 var argv = require('yargs')
 	.usage('Usage: $0')
 	.alias('v', 'verbose')
+	.alias('s', 'script')
+	.demand('script')
 	.argv;
 
 var ctx = {};
 ctx.verbose = argv.verbose;
 
+var script = argv.script;
+
 console.log('');
-console.log('POSTPUBLISH: started');
+console.log('%s: started', script);
 console.log('');
 
-require('../scripts/postpublish.js').call(ctx, function() {
+var scriptPath = path.join(__dirname, '../scripts/', script + '.js');
+if (!fs.existsSync(scriptPath)) {
+	console.error('Could not find step: ' + script + ' at: ' + scriptPath);
+	throw new Error('Step not found');
+}
+
+require(scriptPath).call(ctx, function() {
 	console.log('');
-	console.log('POSTPUBLISH: finished');
+	console.log('%s: finished', script);
 	console.log('');
 });
